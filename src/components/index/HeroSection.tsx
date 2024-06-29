4965858e77a0ce4c05093180d71168c5bf22531c
Łukasz Blachnicki <lblachnicki@gmail.com>
2024-06-29 14:33:08 +0200

    Add embedded testimonials

--- src/components/index/HeroSection.tsx
+++ src/components/index/HeroSection.tsx
@@ -1,27 +1,26 @@
 import React from "react";
-import {useTranslation} from "gatsby-plugin-react-i18next";
+import {Trans, useTranslation} from "gatsby-plugin-react-i18next";
 import LinkButton from "../common/LinkButton";
-import UnderlineWave from "../../images/underline-wave.svg";
 import {StaticImage} from "gatsby-plugin-image";
 
 const HeroSection: React.FC = () => {
     const {t} = useTranslation();
 
-    return <section className="mt-10 md:mt-20">
-        <div className="container mx-auto flex items-center justify-between px-4 md:flex-row">
+    return <section className="px-4 py-20 text-center md:py-40">
+        <div className="container mx-auto flex items-end justify-between px-4 md:flex-row md:justify-start md:gap-56">
             <div className="text-center md:text-left">
                 <div className="mb-8 flex flex-col items-center md:items-start">
-                    <h1 className="text-3xl font-bold md:text-5xl">{t('heroSection.titleTop')}</h1>
-                    <h1 className="relative text-3xl font-bold md:text-5xl">
-                        <span>{t('heroSection.titleBottom')}</span><UnderlineWave
-                        className="absolute -bottom-1/3 -left-2 w-14"/></h1>
+                    <h1 className="text-3xl font-bold sm:whitespace-nowrap md:text-5xl">{t('heroSection.titleTop')}</h1>
+                    <h1 className="text-3xl font-bold sm:whitespace-nowrap md:text-5xl"><Trans i18nKey="heroSection.titleBottom">
+                        <span className="whitespace-nowrap"></span>
+                    </Trans></h1>
                 </div>
                 <p className="text-sm text-gray-500 md:text-base">{t('heroSection.description1')}</p>
                 <p className="mb-8 text-sm text-gray-500 md:text-base">{t('heroSection.description2')}</p>
                 <LinkButton href={"#courses"}>{t('heroSection.chooseClasses')}</LinkButton>
             </div>
             <div className={"hidden md:block"}>
-                <StaticImage src={"../../images/hero.svg"} alt={"Hero image"} width={256}/>
+                <StaticImage src={"../../images/hero.svg"} alt={"Hero image"} width={280}/>
             </div>
         </div>
     </section>
--- src/components/index/TestimonialsSection.tsx
+++ src/components/index/TestimonialsSection.tsx
@@ -1,9 +1,16 @@
 import {useTranslation} from "gatsby-plugin-react-i18next";
 import React from "react";
-import {StaticImage} from "gatsby-plugin-image";
 
 const TestimonialsSection: React.FC = () => {
     const { t } = useTranslation();
+
+    const facebookReviewUrls: string[] = [
+        'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmag.ja.5%2Fposts%2Fpfbid0A5faH4WxzxQHtg71jZrHNsyLa868FS3Qo7zQwyDM6hKwd9d124B6q4HziVVo2mfHl&show_text=true&width=500',
+        'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02EmAKtuXdLP7m521JparrTKGmpEA5agghoZ9tvHsDLZmSvEzHjGAPkxMAPnDUdSGKl%26id%3D100005922067312&show_text=true&width=500',
+        'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02ZQzmxXw5m9LUHRzRXTyDJiaKmHSeHfPTqjPLvCGcS5q4LYwvj7R2srf1DyA3BNzkl%26id%3D100081119022550&show_text=true&width=500',
+        'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Femilia.janowik.5%2Fposts%2Fpfbid0VwcbY36qHMcyP7w3nM9cTjabdQtUs7iHjTyAys1xpDWmSKzfLPKgj2Cxwgx5xKTXl&show_text=true&width=500',
+        'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid0pC5XgHzX39QSftMEEM1ob47X8gChSH7d7a2wgxh9s486MQowTBDo6yH9JxpmQKjsl%26id%3D100088231114689&show_text=true&width=500',
+    ]
 
     return (
         <section className="mt-20 bg-amber-50 px-4 py-20 text-center md:mt-40 md:py-40">
@@ -12,15 +19,18 @@ const TestimonialsSection: React.FC = () => {
                 <p className="mb-8 text-sm md:text-base">{t('testimonialsSection.description')}</p>
 
                 <div className="flex flex-wrap justify-center gap-4 md:gap-8">
-                    <div className="overflow-hidden rounded-xl shadow">
-                        <StaticImage src="../../images/testimonials/1.jpg" alt="Opinia 1"/>
-                    </div>
-                    <div className="overflow-hidden rounded-xl shadow">
-                        <StaticImage src="../../images/testimonials/2.jpg" alt="Opinia 2"/>
-                    </div>
-                    <div className="overflow-hidden rounded-xl shadow">
-                        <StaticImage src="../../images/testimonials/3.jpg" alt="Opinia 3"/>
-                    </div>
+                    {
+                        facebookReviewUrls.map((url, i) => <div key={i} className="overflow-hidden rounded-xl shadow">
+                            <iframe
+                                src={url}
+                                width="500" height="220" style={{border: 'none', overflow: 'hidden'}} scrolling="no"
+                                frameBorder="0"
+                                allowFullScreen={true}
+                                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        </div>)
+                    }
+
+
                 </div>
             </div>
         </section>
