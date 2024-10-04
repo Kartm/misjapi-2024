3f41edc87dca46080b037f3754eaa168188653e7
Łukasz Blachnicki <lblachnicki@gmail.com>
2024-10-04 11:41:54 +0200

    Add close button

--- src/components/common/IframeDialog.tsx
+++ src/components/common/IframeDialog.tsx
@@ -1,5 +1,6 @@
-import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react'
+import {Dialog, DialogBackdrop, DialogPanel, DialogTitle} from '@headlessui/react'
 import React, {PropsWithChildren} from 'react'
+import CloseIcon from '../../images/close.svg';
 
 // .activenow-form #active-now-submit {
 //     margin-left: auto;
@@ -30,6 +31,8 @@ const IframeDialog: React.FC<PropsWithChildren<IframeDialogProps>> = ({open, onC
                         transition
                         className="relative overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
                     >
+                        <DialogTitle className="flex w-full p-4"><button className="ml-auto" onClick={onClose}><CloseIcon className="w-8" /></button></DialogTitle>
+
                         <div className="bg-white p-4 sm:p-6">
                             <div className="flex items-center justify-center">
                                 {children}
